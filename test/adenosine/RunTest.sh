5da317e5818d2a950816ecd7c585022415008ca6
Daniel R. Roe <daniel.r.roe@gmail.com>
2025-11-21 08:32:45 -0500

    Fix residue name. Finish test

--- test/adenosine/RunTest.sh
+++ test/adenosine/RunTest.sh
@@ -10,4 +10,12 @@ if [ ! -f "$MODXNA" ] ; then
 fi
 
 $MODXNA -i modxna.in -m ADE > modxna.out
+ERR=$?
+NDIFF=`diff ADE.lib.save ADE.lib | wc -l`
+((ERR = $ERR + $NDIFF))
+if [ $ERR -eq 0 ] ; then
+  echo "Passed."
+else
+  echo "Failed. Check diff ADE.lib.save ADE.lib"
+fi
 exit $?
